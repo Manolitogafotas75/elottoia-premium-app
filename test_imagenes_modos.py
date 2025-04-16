2ee9a83731154b84d878a4f53ab21c329452baf9
Manolitogafotas75 <unmagma@gmail.com>
2025-04-17 00:15:17 +0200

    Add files via upload

---
+++ test_imagenes_modos.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("Test de imágenes de modos de generación")
+
+st.image("aleatoriobarra.png", caption="Aleatorio", width=150)
+st.image("frecuenciabarra.png", caption="Frecuencia", width=150)
+st.image("hibridobarra.png", caption="Híbrido", width=150)
