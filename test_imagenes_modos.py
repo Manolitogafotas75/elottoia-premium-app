26167fc613a1d5c80728adfd39fb5dcafbe43c3a
Manolitogafotas75 <unmagma@gmail.com>
2025-04-17 01:45:51 +0200

    Delete test_imagenes_modos.py

--- test_imagenes_modos.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.title("Test de imágenes de modos de generación")
-
-st.image("aleatoriobarra.png", caption="Aleatorio", width=150)
-st.image("frecuenciabarra.png", caption="Frecuencia", width=150)
-st.image("hibridobarra.png", caption="Híbrido", width=150)
